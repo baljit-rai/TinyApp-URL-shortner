bda5c042334670e99f71f079801f8b10d29bcc16
Baljit Rai <bsrai91@gmail.com>
2017-09-13 18:04:51 +0000

    intial push for tiny app url shortner, had to start over because my directories got messed up so thats why this is such a big push

---
+++ express_server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port
+var ejs = require("ejs");
+
+app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shorturl", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shorturl,
+    targetURL: urlDatabase[req.params.shorturl]
+  };
+  console.log(urlDatabase[req.params.shorturl])
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // debug statement to see POST parameters
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
